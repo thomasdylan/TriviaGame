85669474a6d41c1934ed450b1c4b4c43ac690958
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-11 17:57:19 -0600

    Finished questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,36 +4,36 @@ var trivia = {
         question: "Who is the toughest man in Letterkenny?",
         options: ["Wayne", "McMurray", "Stewart", "Reilly"],
         answer: "Wayne",
-        correctGif: '<img src= >',
-        incorrectGif: '<img src= >'
+        correctGif: '<img src= https://media.giphy.com/media/X81ZDf5BX9zkolIdWY/giphy.gif>',
+        incorrectGif: '<img src= https://media.giphy.com/media/l0CLV3syoj2L1emc0/giphy.gif>'
     },
     q2: {
         question: "What is the name of Pastor Glen's Christian post-alt rock emo super band",
-        options: ["The Salty Treats", "My Tattered Journal"],
+        options: ["The Salty Treats", "My Tattered Journal", "Sad Gospel", "Convert the Skids"],
         answer: "My Tattered Journal",
         correctGif: '<img src= https://media.giphy.com/media/VFeWMIKOJzEvv6EZEw/giphy.gif>',
-        incorrectGif: '<img src= >'
+        incorrectGif: '<img src= https://media.giphy.com/media/3o6ZtjpIk4erzIQMYU/giphy.gif>'
     },
     q3: {
-        question: "",
-        options: [],
-        answer: "",
-        correctGif: '<img src= >',
-        incorrectGif: '<img src= >'
+        question: "What was the hockey coach sick of leaving droppings on the golf course?",
+        options: ["Gophers", "Rabbits", "Humans", "Canadian Gooses"],
+        answer: "Canadian Gooses",
+        correctGif: '<img src= https://media.giphy.com/media/3oxHQhIsrPBmlFEXuw/giphy.gif>',
+        incorrectGif: '<img src= https://media.giphy.com/media/5n7sUps5DE2CE1d9Ye/giphy.gif>'
     },
     q4: {
-        question: "",
-        options: [],
-        answer: "",
-        correctGif: '<img src= >',
-        incorrectGif: '<img src= >'
+        question: 'Who do the gang throw a "Super Soft Birthday Party" for?',
+        options: ["Katy", "Dan", "Daryl", "Stuart"],
+        answer: "Daryl",
+        correctGif: '<img src= https://media.giphy.com/media/3ornjJP6f70mvFfRXG/giphy.gif>',
+        incorrectGif: '<img src= https://media.giphy.com/media/etKDScqDzX8Bi8KWfs/giphy.gif>'
     },
     q5: {
-        question: "",
-        options: [],
-        answer: "",
-        correctGif: '<img src= >',
-        incorrectGif: '<img src= >'
+        question: "How many people live in Letterkenny?",
+        options: ["1000", "5000", "10000", "500"],
+        answer: "5000",
+        correctGif: '<img src= https://media.giphy.com/media/3ohzdO0qBwFKIrHuQo/giphy.gif>',
+        incorrectGif: '<img src= https://media.giphy.com/media/3ornk2Fk3VegMr4bKw/giphy.gif>'
     }
 };
 
@@ -44,7 +44,7 @@ var timeStarted = false;
 var time = 10;
 
 $("#question").html(currentQuestion);
-$("#image").html(trivia.q2.correctGif);
+$("#image").html(trivia.q3.incorrectGif);
 //10 second timer function
 function tenTimer() {
     if(!timeStarted) {
@@ -62,7 +62,7 @@ function count() {
     }
 }
 
-tenTimer();
+
 
 
 
