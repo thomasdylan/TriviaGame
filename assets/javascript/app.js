6ab5fd14c6fd146fd8ff8f82cb598c9ab3e55fad
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-10 21:05:29 -0600

    Added timer and background.  Started bootstrap styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,40 +4,66 @@ var trivia = {
         question: "Who is the toughest man in Letterkenny?",
         options: ["Wayne", "McMurray", "Stewart", "Reilly"],
         answer: "Wayne",
-        
+        correctGif: '<img src= >',
+        incorrectGif: '<img src= >'
     },
     q2: {
         question: "What is the name of Pastor Glen's Christian post-alt rock emo super band",
         options: ["The Salty Treats", "My Tattered Journal"],
         answer: "My Tattered Journal",
-        correctGif: '<img src= https://media.giphy.com/media/VFeWMIKOJzEvv6EZEw/giphy.gif>'
+        correctGif: '<img src= https://media.giphy.com/media/VFeWMIKOJzEvv6EZEw/giphy.gif>',
+        incorrectGif: '<img src= >'
     },
     q3: {
         question: "",
         options: [],
-        answer: ""
+        answer: "",
+        correctGif: '<img src= >',
+        incorrectGif: '<img src= >'
     },
     q4: {
         question: "",
         options: [],
-        answer: ""
+        answer: "",
+        correctGif: '<img src= >',
+        incorrectGif: '<img src= >'
     },
     q5: {
         question: "",
         options: [],
-        answer: ""
+        answer: "",
+        correctGif: '<img src= >',
+        incorrectGif: '<img src= >'
     }
 };
 
 //Global Variables
 var timer;
 var currentQuestion;
+var timeStarted = false;
+var time = 10;
 
-$("#image").html(trivia.q1.correctGif);
+$("#image").html(trivia.q2.correctGif);
 //10 second timer function
 function tenTimer() {
-    
+    if(!timeStarted) {
+        timeStarted = true;
+        timer = setInterval(count, 1000);
+    }
 }
+
+function count() {
+    time--;
+    $("#timer").html(time);
+    if(time === 0) {
+        clearInterval(timer);
+        timeStarted = false;
+    }
+}
+
+tenTimer();
+
+
 
 
 
